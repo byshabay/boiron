228764512aacf61d74e1d6862c67c8b79ca2744d
Alyona Shabaykina <shabbaykina@mail.ru>
2021-11-08 11:36:12 +0300

    Add fonts

---
+++ inc/header.php
@@ -0,0 +1,44 @@
+<!-- ROOT_URL -->
+<?php $ROOT_URL = 'http://study.ivit.pro/boiron.ru/' ?>
+
+
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>boiron.ru</title>
+
+    <!-- SELF-WRITTEN STYLES -->
+    <link rel="stylesheet" href="<?= $ROOT_URL . 'css/style.css' ?>">
+</head>
+
+<body>
+    <header class="header">
+
+        <!-- 1.TOP MENU START -->
+
+        <div class="header__top">
+            <ul class="container">
+                <li class="header__top-li">
+                    <a class="common-link_white-icon" href="#">
+                        Специалисты в области здравоохранения
+                    </a>
+                </li>
+                <li class="header__top-li header__top-parent">
+                    <a href="#">
+                        Ты?
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+
+        <!-- 1.TOP MENU END -->
+    </header>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!-- 1.HEADER START -->
+<?php include('inc/header.php') ?>
+<!-- 1.HEADER END -->
+
+<!-- 2.MAIN START -->
+
+<h1>test</h1>
+<!-- 2.MAIN END -->
+
+<!-- 3.FOOTER START -->
+
+<!-- 3.FOOTER END -->
