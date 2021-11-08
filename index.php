2ab3024a0840cd3a872473328e8ab575160b3809
Alyona Shabaykina <shabbaykina@mail.ru>
2021-11-08 15:56:55 +0300

    Add common banner + fix header font size

--- index.php
+++ index.php
@@ -4,7 +4,24 @@
 
 <!-- 2.MAIN START -->
 
-<h1>test</h1>
+<main class="main">
+    <!-- 2.1.BANNER START -->
+
+    <section class="common-banner product-banner">
+        <div class="container">
+            <a href="<?= $ROOT_URL ?>" class="back-btn">
+                <img src="<?= $ROOT_URL . 'images/back-arrow.svg' ?>">
+            </a>
+            <h1 class="common-h1">
+                наши продукты
+            </h1>
+        </div>
+
+    </section>
+
+    <!-- 2.1.BANNER END -->
+</main>
+
 <!-- 2.MAIN END -->
 
 <!-- 3.FOOTER START -->
